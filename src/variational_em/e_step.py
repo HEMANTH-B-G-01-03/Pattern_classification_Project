a03820c0b018c67496d6126ecf1b1deaa9a65a84
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-01 23:48:17 +0530

    creatin an e-step

---
+++ src/variational_em/e_step.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class VariationalEStep:
+
+    def __init__(
+        self,
+        n_candidates=5
+    ):
+        self.n_candidates = n_candidates
+
+    def run(
+        self,
+        X,
+        centers,
+        sigma2
+    ):
+
+        n_samples = X.shape[0]
+
+        K_sets = []
+        responsibilities = []
+
+        for i in range(n_samples):
+
+            point = X[i]
+
+            distances = np.sum(
+                (centers - point) ** 2,
+                axis=1
+            )
+
+            nearest_idx = np.argsort(
+                distances
+            )[:self.n_candidates]
+
+            K_sets.append(
+                nearest_idx
+            )
+
+            selected_distances = (
+                distances[nearest_idx]
+            )
+
+            probs = np.exp(
+                -selected_distances
+                /
+                (2 * sigma2)
+            )
+
+            probs = probs / np.sum(
+                probs
+            )
+
+            responsibilities.append(
+                probs
+            )
+
+        return (
+            K_sets,
+            responsibilities
+        )
